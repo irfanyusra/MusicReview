0da55c9cf2fbdda3f72c7a5ab846be88d5e933e4
Yusra Irfan <yusrairfan99@hotmail.com>
2019-11-30 00:49:55 -0500

    email verification

--- Node/models/user.model.js
+++ Node/models/user.model.js
@@ -5,12 +5,10 @@ const Schema = mongoose.Schema;
 
 let userSchema = new Schema({
   //id from mongo 
-  email: { type: String, required: true, max: 100 },
+  email: { type: String, unique: true, required: true, max: 100 },
   name: { type: String, required: true, max: 100 },
   hashPassword: { type: String, required: true },
-  isAdmin: { type: Boolean, default:false },
-  // password: { type: String, required: true, max: 100 },
-
+  isAdmin: { type: Boolean, default: false },
   verified: { type: Boolean, default: false },
   isActive: { type: Boolean, default: true },
 
